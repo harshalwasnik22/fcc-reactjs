eb375215ef7a433a68dfe54243b18a9a01f11135
harshalwasnik22 <hwasnik22@gmail.com>
2023-03-15 19:31:44 +0530

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,5 @@
 import React from 'react'
 import './Navbar.css'
-import './Navbar1'
-// const Navbar = () => {
-//   return (
-//     <>
-//     <header class="cd-header">
-// 		<div class="header-wrapper">
-// 			<div class="logo-wrap">
-// 				<a href="#" class="hover-target"><span>your</span>logo</a>
-// 			</div>
-// 			<div class="nav-but-wrap">
-// 				<div class="menu-icon hover-target">
-// 					<span class="menu-icon__line menu-icon__line-left"></span>
-// 					<span class="menu-icon__line"></span>
-// 					<span class="menu-icon__line menu-icon__line-right"></span>
-// 				</div>					
-// 			</div>					
-// 		</div>				
-// 	</header>
-
-// 	<div class="nav">
-// 		<div class="nav__content">
-// 			<ul class="nav__list">
-// 				<li class="nav__list-item active-nav"><a href="#" class="hover-target">home</a></li>
-// 				<li class="nav__list-item"><a href="#" class="hover-target">studio</a></li>
-// 				<li class="nav__list-item"><a href="#" class="hover-target">news</a></li>
-// 				<li class="nav__list-item"><a href="#" class="hover-target">contact</a></li>
-// 			</ul>
-// 		</div>
-// 	</div>		
-//     </>
-//   )
-// }
-
-// export default Navbar
-
-
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -47,6 +11,7 @@ const Navbar = () => {
 
   return (
     <>
+      <div className={`${navActive ? 'nav-active' : ''}`}>
       <header className="cd-header">
         <div className="header-wrapper">
           <div className="logo-wrap">
@@ -64,15 +29,16 @@ const Navbar = () => {
         </div>
       </header>
 
-      <div className={`nav ${navActive ? 'nav-active' : ''}`}>
+      <div className="nav">
         <div className="nav__content">
-        <ul class="nav__list">
-          <li class="nav__list-item active-nav"><a href="#" class="hover-target">home</a></li>
-          <li class="nav__list-item"><a href="#" class="hover-target">studio</a></li>
-          <li class="nav__list-item"><a href="#" class="hover-target">news</a></li>
-          <li class="nav__list-item"><a href="#" class="hover-target">contact</a></li>
-			</ul>
+        <ul className="nav__list">
+          <li className="nav__list-item active-nav"><a href="#" className="hover-target">home</a></li>
+          <li className="nav__list-item"><a href="#" className="hover-target">studio</a></li>
+          <li className="nav__list-item"><a href="#" className="hover-target">news</a></li>
+          <li className="nav__list-item"><a href="#" className="hover-target">contact</a></li>
+			  </ul>
         </div>
+      </div>
       </div>
     </>
   );
